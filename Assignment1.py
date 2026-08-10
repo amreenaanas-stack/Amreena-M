885c9867cab318bbf4f194ca1a5bc8589c90b949
amreenaanas-stack <amreenaanas@gmail.com>
2026-08-10 13:10:41 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,21 @@
+#ASSIGNMENT 1=
+#print first 10 numbers in fabonacci series ?
+# a=0
+# b=1
+# for i in range (10):
+#  print(a, end=" ")
+#  a, b = b, a + b
+
+ #ASSIGNMENT 2=
+#apple triangle ?
+
+
+#ASSIGNMENT 3=
+
+# n = int(input("Enter number of rows: "))
+# num = 1
+# for i in range(1, n + 1):
+#         for j in range (i):
+#           print(num, end=" ")
+#           num += 1
+#         print()
