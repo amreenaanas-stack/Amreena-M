5b7ef047ff78fe376e14660e906df0d4681487a2
amreenaanas-stack <amreenaanas@gmail.com>
2026-08-10 13:17:17 +0530

    Add files via upload

---
+++ callmd.py
@@ -0,0 +1,18 @@
+
+import md
+print(md.name())
+print(md.add2(2,3))
+
+from md import name, add2
+print(name())
+print(add2(7,8))
+
+
+
+import math
+print(math.sqrt(25))
+print(math.factorial(5))
+print(math.pi)
+
+a=6.3
+print(math.floor(a))
